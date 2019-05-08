c648c8d1d10059ec5f99f6e493c31dca2cff8e81
val <kudatawira@gmail.com>
2019-05-08 02:18:52 +0200

    v2

--- src/testdomeexercises/MergeNames.java
+++ src/testdomeexercises/MergeNames.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Implement the uniqueNames method. When passed two arrays of names, 
+ *it will return an array containing the names that appear in either or both arrays. 
+ *The returned array should have no duplicates.
+ *For example, calling MergeNames.uniqueNames(new String[]{'Ava', 'Emma', 'Olivia'}, new String[]{'Olivia', 'Sophia', 'Emma'})
+ *should return an array containing Ava, Emma, Olivia, and Sophia in any order.
  */
 package testdomeexercises;
 
@@ -9,7 +11,21 @@ package testdomeexercises;
  *
  * @author kudat
  */
+import java.util.*; 
+import java.util.stream.*; 
 public class MergeNames {
     
+    public static String[] uniqueNames(String[] names1, String[] names2) {
+        Stream<String> first = Stream.of(names1);
+        Stream<String> second = Stream.of(names2);
+        Stream<String> result = Stream.concat(first,second).distinct();
+        String[] unique = result.toArray(String[]::new);
+        return unique;
+     }
+    
+    public static void main(String[] args) {
+        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
 }
-
---
+++ src/testdomeexercises/Palindrome.java
@@ -0,0 +1,29 @@
+/*
+ *A recursive version of the Palindrome question 
+ */
+package testdomeexercises;
+
+/**
+ *
+ * @author kudat
+ */
+public class Palindrome {
+    public static boolean isPalindrome(String word) {
+      
+        if(word.length() == 0 || word.length() == 1)
+        {
+            return true;
+        }
+        if(Character.toLowerCase(word.charAt(0)) == Character.toLowerCase(word.charAt(word.length()-1)))
+        {
+            return (isPalindrome(word.substring(1,word.length()-1)));
+        }
+        
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("Deleveled"));
+    }
+}
+
